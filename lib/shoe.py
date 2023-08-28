8814e81d9d6e08b38e34747ca7512667c56980b9
Ann Ndiga <annndiga@gmail.com>
2023-08-28 17:34:39 +0300

    Update

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+    def cobble(self):
+       if not isinstance(self.size, int):
+            error_message = f"size must be an integer for shoe '{self.brand}', got type {type(self.size).__name__}"
+            raise TypeError(error_message)
+       print("Your shoe is as good as new!")
+       self.condition = "New"
+
+       
+        
+       
+
+        
